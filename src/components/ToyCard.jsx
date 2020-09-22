06cf432b499acf75ff0d21b3be5a880dffa31439
cortezd334 <cortezd334@gmail.com>
2020-09-22 09:37:41 -0700

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 class ToyCard extends Component {
+  
+  clickHandler = (e) => {
+    this.props.del(this.props.toy.id)
+    // debugger
+    // e.target.parentElement.remove()
+  }
 
   render() {
+    const {name, image, likes} = this.props.toy
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
+        <h2>{name}</h2>
+        <img src={image} img alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <button className="del-btn" onClick={this.clickHandler}>Donate to GoodWill</button >
       </div>
     );
   }
@@ -17,3 +24,31 @@ class ToyCard extends Component {
 }
 
 export default ToyCard;
+
+
+
+
+
+
+
+
+
+
+// class ToyCard extends Component {  
+//   render() {
+//     let {name, image, likes} = this.props.toy
+//     console.log(this.props.toy)
+//     return (
+//       <div className="card">
+//         <h2>{name}</h2>
+//         <img src={image} alt={name} className="toy-avatar" />
+//         <p>{likes} Likes </p>
+//         <button className="like-btn">Like {'<3'}</button>
+//         <button className="del-btn" onClick={this.props.delete(this.props.toy)}>Donate to GoodWill</button>
+//       </div>
+//     );
+//   }
+
+// }
+
+// export default ToyCard;
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state = {
+    name: '',
+    image: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.newToy(this.state.name, this.state.image)
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" value={this.state.name} placeholder="Enter a toy's name..." className="input-text" onChange={this.handleChange}/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" value={this.state.image}placeholder="Enter a toy's image URL..." className="input-text" onChange={this.handleChange}/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
@@ -18,5 +33,4 @@ class ToyForm extends Component {
   }
 
 }
-
 export default ToyForm;
